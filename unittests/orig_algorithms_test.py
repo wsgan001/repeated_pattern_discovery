07cfa887bf95b5617e26184dfe950c8b932d3f99
Otso Björklund <otso.bjorklund@gmail.com>
2017-11-07 15:25:58 +0200

    Renamed files and class

--- unittests/orig_algorithms_test.py
+++ unittests/orig_algorithms_test.py
@@ -1,15 +1,15 @@
 import unittest
 from dataset import Dataset
 from vector import Vector
-import algorithms
+import orig_algorithms
 from tec import TEC
 
 
-class AlgorithmsTest(unittest.TestCase):
+class OrigAlgorithmsTest(unittest.TestCase):
 
     def test_sia(self):
         dataset = Dataset('unittest_data/test_data1.csv')
-        result = algorithms.sia(dataset)
+        result = orig_algorithms.sia(dataset)
 
         exp_res = []
         exp_res.append((Vector([2, 1]), [Vector([1, 2]), Vector([2, 1])]))
@@ -17,17 +17,17 @@ class AlgorithmsTest(unittest.TestCase):
         exp_res.append((Vector([3, 0]), [Vector([1, 2])]))
         exp_res.append((Vector([1, 2]), [Vector([2, 1])]))
 
-        results_match = AlgorithmsTest.check_result(result, exp_res)
+        results_match = OrigAlgorithmsTest.check_result(result, exp_res)
         if not results_match:
             print('Incorrect result')
-            algorithms.print_mtps(result)
+            orig_algorithms.print_mtps(result)
 
         self.assertTrue(results_match)
 
     def test_sia_with_data_from_meredith(self):
         dataset = Dataset('unittest_data/Meredith2002_fig11.csv')
-        result = algorithms.sia(dataset)
-        algorithms.print_mtps(result)
+        result = orig_algorithms.sia(dataset)
+        orig_algorithms.print_mtps(result)
 
         exp_res = []
         exp_res.append((Vector([0, 1]), [Vector([2, 1]), Vector([2, 2])]))
@@ -40,19 +40,19 @@ class AlgorithmsTest(unittest.TestCase):
         exp_res.append((Vector([2, -1]), [Vector([1, 3])]))
         exp_res.append((Vector([2, 1]), [Vector([1, 1])]))
 
-        results_match = AlgorithmsTest.check_result(result, exp_res)
+        results_match = OrigAlgorithmsTest.check_result(result, exp_res)
         if not results_match:
             print('Incorrect result')
-            algorithms.print_mtps(result)
+            orig_algorithms.print_mtps(result)
 
         self.assertTrue(results_match)
 
     def test_siatec_with_data_from_meredith(self):
         dataset = Dataset('unittest_data/Meredith2002_fig11.csv')
-        result = algorithms.siatec(dataset)
+        result = orig_algorithms.siatec(dataset)
 
-        exp_res = AlgorithmsTest.get_tecs_for_Meredith2002_fig11()
-        results_match = AlgorithmsTest.check_result(result, exp_res)
+        exp_res = OrigAlgorithmsTest.get_tecs_for_Meredith2002_fig11()
+        results_match = OrigAlgorithmsTest.check_result(result, exp_res)
         if not results_match:
             print('Incorrect result')
 
@@ -68,15 +68,15 @@ class AlgorithmsTest(unittest.TestCase):
 
     def test_cosiatec_with_data_from_meredith2002(self):
         dataset = Dataset('unittest_data/Meredith2002_fig11.csv')
-        best_tecs = algorithms.cosiatec(dataset)
-        all_tecs = AlgorithmsTest.get_tecs_for_Meredith2002_fig11()
+        best_tecs = orig_algorithms.cosiatec(dataset)
+        all_tecs = OrigAlgorithmsTest.get_tecs_for_Meredith2002_fig11()
 
-        algorithms.print_tecs(all_tecs)
-        algorithms.print_tecs(best_tecs)
+        orig_algorithms.print_tecs(all_tecs)
+        orig_algorithms.print_tecs(best_tecs)
 
         # Check that all tecs in best_tecs are valid tecs.
         for tec in best_tecs:
-            self.assertTrue(AlgorithmsTest.tec_in_list(tec, all_tecs))
+            self.assertTrue(OrigAlgorithmsTest.tec_in_list(tec, all_tecs))
 
         # Check coverage of best_tecs.
         covered_points = []
@@ -91,15 +91,15 @@ class AlgorithmsTest(unittest.TestCase):
 
     def test_cosiatec_with_random_data(self):
         dataset = Dataset('unittest_data/rand_patterns.csv')
-        all_tecs = algorithms.siatec(dataset)
-        algorithms.print_tecs(all_tecs)
+        all_tecs = orig_algorithms.siatec(dataset)
+        orig_algorithms.print_tecs(all_tecs)
 
-        best_tecs = algorithms.cosiatec(dataset)
-        algorithms.print_tecs(best_tecs)
+        best_tecs = orig_algorithms.cosiatec(dataset)
+        orig_algorithms.print_tecs(best_tecs)
 
         # Check that all tecs in best_tecs are valid tecs.
         for tec in best_tecs:
-            self.assertTrue(AlgorithmsTest.tec_in_list(tec, all_tecs))
+            self.assertTrue(OrigAlgorithmsTest.tec_in_list(tec, all_tecs))
 
         # Check coverage of best_tecs.
         covered_points = []
@@ -114,15 +114,15 @@ class AlgorithmsTest(unittest.TestCase):
 
     def test_siatec_compress_with_data_from_meredith2002(self):
         dataset = Dataset('unittest_data/Meredith2002_fig11.csv')
-        best_tecs = algorithms.siatec_compress(dataset)
-        all_tecs = AlgorithmsTest.get_tecs_for_Meredith2002_fig11()
+        best_tecs = orig_algorithms.siatec_compress(dataset)
+        all_tecs = OrigAlgorithmsTest.get_tecs_for_Meredith2002_fig11()
 
-        algorithms.print_tecs(all_tecs)
-        algorithms.print_tecs(best_tecs)
+        orig_algorithms.print_tecs(all_tecs)
+        orig_algorithms.print_tecs(best_tecs)
 
         # Check that all tecs in best_tecs are valid tecs.
         for tec in best_tecs:
-            self.assertTrue(AlgorithmsTest.tec_in_list(tec, all_tecs))
+            self.assertTrue(OrigAlgorithmsTest.tec_in_list(tec, all_tecs))
 
         # Check coverage of best_tecs.
         covered_points = []
@@ -137,15 +137,15 @@ class AlgorithmsTest(unittest.TestCase):
 
     def test_siatec_compress_with_random_data(self):
         dataset = Dataset('unittest_data/rand_patterns.csv')
-        all_tecs = algorithms.siatec(dataset)
-        algorithms.print_tecs(all_tecs)
+        all_tecs = orig_algorithms.siatec(dataset)
+        orig_algorithms.print_tecs(all_tecs)
 
-        best_tecs = algorithms.siatec_compress(dataset)
-        algorithms.print_tecs(best_tecs)
+        best_tecs = orig_algorithms.siatec_compress(dataset)
+        orig_algorithms.print_tecs(best_tecs)
 
         # Check that all tecs in best_tecs are valid tecs.
         for tec in best_tecs:
-            self.assertTrue(AlgorithmsTest.tec_in_list(tec, all_tecs))
+            self.assertTrue(OrigAlgorithmsTest.tec_in_list(tec, all_tecs))
 
         # Check coverage of best_tecs.
         covered_points = []
@@ -202,7 +202,7 @@ class AlgorithmsTest(unittest.TestCase):
 
     def test_vec(self):
         pattern = [Vector([0, 0]), Vector([0, 2]), Vector([1, 1])]
-        vec_pattern = algorithms.vec(pattern)
+        vec_pattern = orig_algorithms.vec(pattern)
         self.assertEqual(vec_pattern, [Vector([0, 2]), Vector([1, -1])])
 
 
