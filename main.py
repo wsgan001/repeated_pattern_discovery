30d0711190fb4d2fca47f93557fb2f526f0c80bc
Otso Björklund <otso.bjorklund@gmail.com>
2017-12-19 16:01:29 +0200

    Bach excerpt and printout function

--- main.py
+++ main.py
@@ -35,9 +35,47 @@ def test_siatec_hash_time():
                                      Dataset('testfiles/rand_patterns_' + str(i) + '_dim2.csv'), 2)
 
 
+def print_examples():
+    dataset = Dataset('testfiles/bach_wtk_excerpt.csv')
+    helpers.print_mtps(orig_algorithms.sia(dataset))
+    helpers.print_tecs(orig_algorithms.siatec(dataset))
+
+
+def simplified_vec_str(vec):
+    comps = []
+    for k in range(vec.dimensionality()):
+        comps.append(str(int(vec[k])))
+
+    return '(' + ','.join(comps) + ')'
+
+
+def print_v_for_dataset(dataset):
+
+    dataset.sort_ascending()
+    table_lines = [['\ ']]
+
+    for i in range(len(dataset)):
+        table_lines[0].append('$' + simplified_vec_str(dataset[i]) + '$')
+
+    for i in range(len(dataset)):
+        table_lines.append(['$' + simplified_vec_str(dataset[i]) + '$'])
+        for j in range(len(dataset)):
+            if i < j:
+                table_lines[i + 1].append('$\\langle ' + simplified_vec_str(dataset[j] - dataset[i]) + ', ' + str(i) + '\\rangle $')
+            else:
+                table_lines[i + 1].append('\\ ')
+
+    pos_string = '|'.join(['c' for v in dataset])
+    print('\\begin{tabular}[pos]{' + pos_string + '|c}')
+
+    for i in range(len(table_lines)):
+        print('\t' + ' & '.join(table_lines[i]) + '\\\\')
+
+    print('\\end{tabular}')
+
+
 def main():
-    test_sia_hash_time()
-    test_siatec_hash_time()
+    print_v_for_dataset(Dataset('testfiles/bach_wtk_excerpt.csv'))
 
 
 if __name__ == "__main__":
