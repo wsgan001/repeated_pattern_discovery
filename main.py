a807e91c7861ff4eafa0836bf4d22b99cb991021
Otso Björklund <otso.bjorklund@gmail.com>
2018-02-15 13:08:27 +0200

    Ready for runs on mtp_count_min datasets

--- main.py
+++ main.py
@@ -29,12 +29,18 @@ def measure_tec_algorithms_on_datasets(datasets, datasets_name):
 
 
 def measure_tec_algorithm_runtimes():
-    rand_patterns = dataset_generation.get_random_patterns_set()
-    mtp_count_max = dataset_generation.get_mtp_count_max_dataset()
-    mtp_count_min = dataset_generation.get_mtp_count_min_dataset()
-    measure_tec_algorithms_on_datasets(rand_patterns, 'rand_patterns')
-    measure_tec_algorithms_on_datasets(mtp_count_max, 'mtp_count_max')
-    measure_tec_algorithms_on_datasets(mtp_count_min, 'mtp_count_min')
+    mtp_count_min1 = {'testfiles/mtp_count_min/mtp_count_min_500.csv': 1,
+                      'testfiles/mtp_count_min/mtp_count_min_1000.csv': 1,
+                      'testfiles/mtp_count_min/mtp_count_min_1500.csv': 1,
+                      'testfiles/mtp_count_min/mtp_count_min_2000.csv': 1,
+                      'testfiles/mtp_count_min/mtp_count_min_2500.csv': 1,
+                      'testfiles/mtp_count_min/mtp_count_min_3000.csv': 1,
+                      'testfiles/mtp_count_min/mtp_count_min_3500.csv': 1}
+
+    mtp_count_min2 = {'testfiles/mtp_count_min/mtp_count_min_4000.csv': 1}
+
+    measure_tec_algorithms_on_datasets(mtp_count_min1, 'mtp_count_min_1')
+    measure_tec_algorithms_on_datasets(mtp_count_min2, 'mtp_count_min_2')
 
 
 def main():
