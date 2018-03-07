cd78569ab76a92eab3c1c57ed074baafd9fdd824
Otso Björklund <otso.bjorklund@gmail.com>
2018-03-07 11:04:51 +0200

    Compute the statistics for compression ratios in the outputs

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import dataset_generation
 from datetime import datetime
 from operator import itemgetter
 import helpers
+import heuristics
 
 
 def measure_siatech_siatechf():
@@ -88,8 +89,63 @@ def measure_filtering_algorithm_outputs():
         outputfile.write(', '.join(output_line) + '\n')
 
 
+def cr_statistics(tecs):
+    min_cr = 1000000
+    max_cr = 0
+    avg = 0
+
+    for tec in tecs:
+        cr = heuristics.compression_ratio(tec)
+        if cr < min_cr:
+            min_cr = cr
+        if cr > max_cr:
+            max_cr = cr
+
+        avg += cr
+
+    avg /= len(tecs)
+
+    return min_cr, max_cr, avg
+
+
+def compute_cr_statistics():
+    datasets = dataset_generation.get_random_patterns_set()
+
+    output_lines = []
+
+    for dataset_name in datasets:
+        dataset = Dataset(dataset_name)
+        siatech_compr_output = orig_algorithms.siatech_compress(dataset)
+        primary, secondary = orig_algorithms.forths_algorithmh(dataset, 15, 0.5)
+        forths_output = primary
+        for sec in secondary:
+            if sec:
+                forths_output += sec
+        cosiatech_output = orig_algorithms.cosiatech(dataset)
+
+        min_cr, max_cr, avg_cr = cr_statistics(siatech_compr_output)
+        output_lines.append([dataset.get_name(), str(len(dataset)), 'siatec_compress', str(min_cr), str(max_cr), str(avg_cr)])
+
+        min_cr, max_cr, avg_cr = cr_statistics(forths_output)
+        output_lines.append([dataset.get_name(), str(len(dataset)), 'forths_algo', str(min_cr), str(max_cr), str(avg_cr)])
+
+        min_cr, max_cr, avg_cr = cr_statistics(cosiatech_output)
+        output_lines.append([dataset.get_name(), str(len(dataset)), 'cosiatec', str(min_cr), str(max_cr), str(avg_cr)])
+
+    # Write to file. Begin by putting the first line.
+    timestamp = '{:%Y-%m-%d_%H-%M-%S}'.format(datetime.now())
+    output_file_name = 'output/' + 'filtering_outputs_' + timestamp + '.csv'
+    outputfile = open(output_file_name, mode='w')
+    outputfile.write('dataset, dataset_size, algorithm, min_cr, max_cr, avg_cr\n')
+
+    output_lines.sort(key=itemgetter(2, 1))
+
+    for output_line in output_lines:
+        outputfile.write(', '.join(output_line) + '\n')
+
+
 def main():
-    measure_siatech_siatechf()
+    compute_cr_statistics()
 
 
 if __name__ == "__main__":
